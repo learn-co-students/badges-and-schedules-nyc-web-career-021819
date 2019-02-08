46d594728fd32d244639daa97b2c8678c10cb572
tchang <tlchang9@gmail.com>
2019-02-07 23:34:33 -0500

    Wrote #badge_maker, #batch_badge_creator, #assign_rooms, and #printer

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list_of_names)
+    name_badges = []
+
+    for name in list_of_names do
+        name_badges.push(badge_maker(name))
+    end
+
+    name_badges
+end
+
+def assign_rooms(list_of_speakers)
+    speaker_rooms = []
+
+    for speaker in list_of_speakers do
+        room_number = list_of_speakers.index(speaker) + 1
+        room_assignment = "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+        speaker_rooms.push(room_assignment)
+    end
+    speaker_rooms
+end
+
+def printer(attendees)
+    for name in batch_badge_creator(attendees)
+        puts name  
+    end
+    
+    for room in assign_rooms(attendees)
+        puts room
+    end
+end
